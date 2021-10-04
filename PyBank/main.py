8e8b0014ad5827aeaccabebdc8326433d6313eb6
P219-C <19741043+P219-C@users.noreply.github.com>
2021-10-04 22:46:40 +0800

    Assignment completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,47 +1,62 @@
+# Author: Pablo Crespo Carrillo
+# Data Analytics Boot Camp
+# The University of Western Australia
+# Third assignment: Python Homework - Py Me Up, Charlie
+
 import os
 import csv
 
+# -----------------------------------------------------------------------
+# Opening the csv file and storing the data in 'csv_list'
 csv_path = os.path.join("Resources", "budget_data.csv")
 
 with open(csv_path) as csv_file:
 
     csv_reader = csv.reader(csv_file, delimiter = ",")
+    # print(f"File correctly read: {csv_reader}")
 
-    print(f"File correctly read: {csv_reader}")
-    csv_header = next(csv_reader)
+    csv_header = next(csv_reader)       #Storing the header row
     # print(f"The header is: {csv_header}")
 
     csv_list = list(csv_reader)
+# ---------------------------------------------------------------------------
 
+# ***************************************************************************
+# Looping through the list to store dates and profits/losses in two separate lists with correct data type
 list_date = [i[0] for i in csv_list]
 list_value = [int(i[1]) for i in csv_list]
+# ***************************************************************************
 
 months_total = len(list_date)
 net_total = sum(list_value)
 
-# calculating the Average Change using a for loop
-average_change = 0
+# //////// calculating the Average of the Changes in Profit/Losses using a for loop ///////
+profit_change = list()
 for i in range(months_total-1):
-    average_change += list_value[i]-list_value[i+1]
-    average_change = average_change/months_total
+    profit_change.append(list_value[i+1] - list_value[i])
+    average_change = sum(profit_change)/(months_total-1)
+# /////////////////////////////////////////////////////////////////////////////////////////
 
-# looping throughthe zipped tuples to find the Greatest Increase and Decrease Values and Dates
-for row in csv_list:
-    if int(row[1]) == max(list_value):
-        # print(row)
-        greatest_inc_value = int(row[1])
-        greatest_inc_date = row[0]
-    elif int(row[1]) == min(list_value):
-        # print(row)
-        greatest_dec_value = int(row[1])
-        greatest_dec_date = row[0]
-    
+greatest_inc_value = max(profit_change)
+greatest_dec_value = min(profit_change)
+
+# +++++++++ Finding the date associated with the greatest increase and decrease in profit ++++++++++++++++++
+profit_change.insert(0,0) #List started with zero because there was no change in profit in the first month
+
+for index, row in enumerate(profit_change):
+    if row == greatest_inc_value:
+        greatest_inc_date = list_date[index]
+    elif row == greatest_dec_value:
+        greatest_dec_date = list_date[index]
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Output message and writing the output file    
 output_message = str(f"""
 Financial Analysis
 ---------------------------------
 Total Months: {months_total}
 Total: ${net_total}
-Average Change: ${average_change:.{7}}
+Average Change: ${average_change:.2f}
 Greatest Increase in Profits: {greatest_inc_date} (${greatest_inc_value})
 Greatest Decrease in Profits: {greatest_dec_date} (${greatest_dec_value})
 """)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,25 +1,36 @@
+# Author: Pablo Crespo Carrillo
+# Data Analytics Boot Camp
+# The University of Western Australia
+# Third assignment: Python Homework - Py Me Up, Charlie
+
 import os
 import csv
 
+# -----------------------------------------------------------------------
+# Opening the csv file and storing the data in 'csv_list'
 csv_path = os.path.join("Resources", "election_data.csv")
 
 with open(csv_path) as csv_file:
 
     csv_reader = csv.reader(csv_file, delimiter = ",")
 
-    print(f"File correctly read: {csv_reader}")
-    csv_header = next(csv_reader)
+    #print(f"File correctly read: {csv_reader}")
+    csv_header = next(csv_reader)       #Storing the header row
 
     csv_list = list(csv_reader)
-    
+# -------------------------------------------------------------------------    
 
+# Looping through the list to store data in individual lists with the correct data type ///////////
 list_voter = [int(i[0]) for i in csv_list]
 list_county = [i[1] for i in csv_list]
 list_candidate = [i[2] for i in csv_list]
+# /////////////////////////////////////////////////////////////////////////////////////////////////
 
 total_votes = len(list_voter)
 
-candidates = list(set(list_candidate))
+candidates = list(set(list_candidate))  # Creating a list of unique candidates with 'set'
+
+# Loop to count votes ..............................................................................
 candidates_votes = [0, 0, 0, 0]
 for row in csv_list:
     if row[2] == candidates[0]:
@@ -30,21 +41,31 @@ for row in csv_list:
         candidates_votes[2] += 1
     elif row[2] == candidates[3]:
         candidates_votes[3] += 1
+# .....................................................................................................
 
-for i in range(3):
+
+for i in range(4):
     if candidates_votes[i] == max(candidates_votes):
         winner = candidates[i]
 
-print(f"""
+output_message = str(f"""
 Election Results
 --------------------------------------------
 Total Votes: {total_votes}
 --------------------------------------------
-{candidates[0]}: {candidates_votes[0]*100/total_votes} ({candidates_votes[0]})
-{candidates[1]}: {candidates_votes[1]*100/total_votes} ({candidates_votes[1]})
-{candidates[2]}: {candidates_votes[2]*100/total_votes} ({candidates_votes[2]})
-{candidates[3]}: {candidates_votes[3]*100/total_votes} ({candidates_votes[3]})
+{candidates[0]}: {candidates_votes[0]*100/total_votes:.3f}% ({candidates_votes[0]})
+{candidates[1]}: {candidates_votes[1]*100/total_votes:.3f}% ({candidates_votes[1]})
+{candidates[2]}: {candidates_votes[2]*100/total_votes:.3f}% ({candidates_votes[2]})
+{candidates[3]}: {candidates_votes[3]*100/total_votes:.3f}% ({candidates_votes[3]})
 --------------------------------------------
 Winner: {winner}
 --------------------------------------------
 """)
+
+print(output_message)
+
+output_path = os.path.join("analysis", "Poll_Analysis.txt")
+
+with open(output_path, 'w') as txtfile:
+
+    txtfile.write(output_message)
